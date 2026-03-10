660cd725cac122580fbeb1f9e29a2bb77d21380b
tekitounix <sicksteen@me.com>
2026-03-10 21:30:51 +0900

    release: umitest v0.3.0 package definition

--- packages/u/umitest/xmake.lua
+++ packages/u/umitest/xmake.lua
@@ -1,13 +1,13 @@
 package("umitest")
-    set_homepage("https://github.com/tekitounix/umi")
-    set_description("UMI zero-macro lightweight test framework for C++23")
+    set_homepage("https://github.com/tekitounix/umitest")
+    set_description("Zero-macro lightweight test framework for C++23")
     set_license("MIT")
 
     set_kind("library", {headeronly = true})
 
     add_urls("https://github.com/tekitounix/umitest/releases/download/v$(version)/umitest-$(version).tar.gz")
     add_versions("dev", "git:../../../../lib/umitest")
-    add_versions("0.1.0", "4c28b5f004dae1a30d24ff7094ac4881177cd40ddbe55971833bc220a924a65d")
+    add_versions("0.3.0", "4f919fd70aeed2ed7183015af2e98af60f22775dc22d2785b6817818596c58d0")
 
     on_install(function(package)
         os.cp("include", package:installdir())
